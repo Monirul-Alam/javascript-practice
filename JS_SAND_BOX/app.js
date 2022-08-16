ac1e8a7c8c4fb24e1ea64bf0ff503849b8be4a66
Monirul Alam <you@example.com monirulcr2325@gmail.com>
2022-08-16 20:39:48 +0600

    Using The Console

--- JS_SAND_BOX/app.js
+++ JS_SAND_BOX/app.js
@@ -1,1 +1,11 @@
- 
+// Using The Console
+console.log(123);
+var greeting = 'hello';
+console.log(greeting);
+console.log([1,2,4,5]);
+console.log({a:3,b:2});
+console.table({a:3,b:2});
+
+console.error('This is error');
+console.clear();
+console.warn('This is warning');
