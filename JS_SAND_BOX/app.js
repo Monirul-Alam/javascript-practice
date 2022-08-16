bc95c00820e8fbc928be02a20f81d316f681eb10
Monirul Alam <you@example.com monirulcr2325@gmail.com>
2022-08-16 21:18:16 +0600

    Variable- var, let, const

--- JS_SAND_BOX/app.js
+++ JS_SAND_BOX/app.js
@@ -1,11 +1,67 @@
 // Using The Console
-console.log(123);
-var greeting = 'hello';
-console.log(greeting);
-console.log([1,2,4,5]);
-console.log({a:3,b:2});
-console.table({a:3,b:2});
+// console.log(123);
+// var greeting = 'hello';
+// console.log(greeting);
+// console.log([1,2,4,5]);
+// console.log({a:3,b:2});
+// console.table({a:3,b:2});
 
-console.error('This is error');
-console.clear();
-console.warn('This is warning');
+// console.error('This is error');
+// console.clear();
+// console.warn('This is warning');
+
+// --------------------------------------------------------------------
+
+// Variable- var, let ,const
+
+
+// var name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// // Init var
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting);
+
+// // letters, numbers, _, $
+// // Can not start with number
+
+// // Multi word vars
+// var firstName = 'John'; // Camel case
+// var first_name = 'Sara'; // Underscore
+// var FirstName = 'Tom'; // Pascal case
+// var firstname;
+
+// LET
+// let name;
+// name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+
+// CONST
+// const name = 'John';
+// console.log(name);
+// Can not reassign
+// name = 'Sara';
+// Have to assign a value
+// const greeting;
+
+const person = {
+  name: 'John',
+  age: 30
+}
+
+// person.name = 'Sara';
+// person.age = 32;
+
+console.log(person);
+
+const numbers = [1,2,3,4,5];
+numbers.push(6);
+
+console.log(numbers);
