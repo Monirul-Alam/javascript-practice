4d7796f5c542cebb29478c06c5a826ddcdd5b924
Monirul Alam <you@example.com monirulcr2325@gmail.com>
2022-09-02 21:13:29 +0600

    Date and Time

--- JS_SAND_BOX/app.js
+++ JS_SAND_BOX/app.js
@@ -295,41 +295,53 @@
 ////////////////////////////////////////////////////////////
 
 // object literal
-const person = {
-  firstName: 'Steve',
-  lastName: 'Smith',
-  age: 36,
-  email: 'steve@aol.com',
-  hobbies: ['music', 'sports'],
-  address: {
-    city: 'Miami',
-    state: 'FL'
-  },
-  getBirthYear: function(){
-    return 2017 - this.age;
-  }
-}
+// const person = {
+//   firstName: 'Steve',
+//   lastName: 'Smith',
+//   age: 36,
+//   email: 'steve@aol.com',
+//   hobbies: ['music', 'sports'],
+//   address: {
+//     city: 'Miami',
+//     state: 'FL'
+//   },
+//   getBirthYear: function(){
+//     return 2017 - this.age;
+//   }
+// }
 
-let val;
+// let val;
 
-val = person;
-// Get specific value
-val = person.firstName;
-val = person['lastName'];
-val = person.age;
-val = person.hobbies[1];
-val = person.address.state;
-val = person.address['city'];
-val = person.getBirthYear();
+// val = person;
+// // Get specific value
+// val = person.firstName;
+// val = person['lastName'];
+// val = person.age;
+// val = person.hobbies[1];
+// val = person.address.state;
+// val = person.address['city'];
+// val = person.getBirthYear();
 
-console.log(val);
+// console.log(val);
 
-const people = [
-  {name: 'John', age: 30},
-  {name: 'Mike', age: 23},
-  {name: 'Nancy', age: 40}
-];
+// const people = [
+//   {name: 'John', age: 30},
+//   {name: 'Mike', age: 23},
+//   {name: 'Nancy', age: 40}
+// ];
 
-for(let i = 0; i < people.length; i++){
-  console.log(people[i].name);
-}
+// for(let i = 0; i < people.length; i++){
+//   console.log(people[i].name);
+// }
+
+///////////////////////////////////////////////////////
+
+// Data and Time
+
+ let val;
+ const today = new Date();
+ 
+
+ val = today;
+
+ console.log( val); // Date is a reference type Object.
