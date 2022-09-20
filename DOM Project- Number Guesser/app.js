0f69363721c426eb2653122dae4d3f6270e58ba6
Monirul Alam <monirulcr2325@gmail.com>
2022-09-20 22:47:00 +0600

    Play Again

--- DOM Project- Number Guesser/app.js
+++ DOM Project- Number Guesser/app.js
@@ -4,7 +4,7 @@
 
 let min =1,
     max= 10,  
-    winningNum =2, 
+    winningNum = getRandomNum(max, min), 
     guessesLeft =3;
     
     
@@ -19,6 +19,14 @@ message = document.querySelector('.message');
 // Assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max;
+
+// play again event listener
+ game.addEventListener('mousedown', function(e){
+  if(e.target.className === 'play-again') {
+    window.location.reload();
+  }
+
+ });
 
 // Listen for guess
 guessBtn.addEventListener('click', function(){
@@ -65,6 +73,15 @@ function gameOver(won, msg) {
    message.style.color = color;
    // set message
    setMessage(msg)
+
+   // Play Again?
+   guessBtn.value = 'Play Again'
+   guessBtn.className += 'play-again'
+}
+
+// get winning Number
+function getRandomNum(max, min ) {
+ return Math.floor(Math.random()*(max-min +1)+min);
 }
 
 // Set message
