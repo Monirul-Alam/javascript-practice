bacdfb7522300102e192d335549fd30645db479f
Monirul Alam <monirulcr2325@gmail.com>
2022-10-06 23:43:51 +0600

    findIndex() and filter() method

--- es6-details/arrayFind()Method.js
+++ es6-details/arrayFind()Method.js
@@ -17,3 +17,19 @@ class Student {
 
 let student = new Student ( " sumit " , 35 ) ;
 student.exampleFunction();
+
+// Array Find Index()
+var numbers = [1,23,4,22, 14,53,21,3445];
+var result = numbers.findIndex((currenrValue, index ,arr) => {
+  return currenrValue>20;
+}); 
+
+//
+var result1 = numbers.filter((currenrValue, index ,arr) => {
+  console.log(index)
+  console.log(arr)
+  return currenrValue>20;
+}); 
+
+console.log(result);
+console.log(result1);
