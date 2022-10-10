f406252242e5af875aa73c40aadecf9c321f66ad
Monirul Alam <monirulcr2325@gmail.com>
2022-10-11 01:14:26 +0600

    map() and push() method

---
+++ es6-details/map().js
@@ -0,0 +1,8 @@
+// Array.prototype.map()
+var numbers = [1,2,4,5,6];
+
+result = numbers.map((num) => {
+  return 2* num;
+});
+
+console.log(result);
---
+++ es6-details/push()Method.js
@@ -0,0 +1,7 @@
+// Array.prototype.push()
+var numbers = [1,2,3,4,5];
+
+var result = numbers.push(5,6,3,3);
+
+console.log(numbers);
+console.log(result);
