9f23c2f553e95f1754dee9297e341c2b963a480a
Monirul Alam <monirulcr2325@gmail.com>
2022-10-13 02:11:35 +0600

    fect api

---
+++ Javascript Fetch/app.js
@@ -0,0 +1,71 @@
+document.getElementById('button1').addEventListener('click', loadCustomer);
+document.getElementById('button2').addEventListener('click', loadCustomers);
+
+function loadCustomer(e) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'customer.json', true);
+  
+  xhr.onload = function() {
+    if(this.status === 200){
+      // console.log(this.responseText);
+
+      const customer = JSON.parse(this.responseText);
+
+      const output = `
+      <ul>
+        <li>ID: ${customer.id}</li>
+        <li>ID: ${customer.name}</li>
+        <li>ID: ${customer.company}</li>
+      
+      </ul>  
+       
+      `;
+
+      document.getElementById('customer').innerHTML = output;
+
+    }
+  }
+
+
+  xhr.send();
+}
+
+//  Load Customers
+
+function loadCustomers (e) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'customers.json', true);
+  
+  xhr.onload = function() {
+    if(this.status === 200){
+      // console.log(this.responseText);
+
+      const customers = JSON.parse(this.responseText);
+
+      let output1 = '';
+
+      customers.forEach(function(customer){
+        output1 +=
+        `
+        <ul>
+        <li>ID: ${customer.id}</li>
+        <li>ID: ${customer.name}</li>
+        <li>ID: ${customer.company}</li>
+      
+      </ul>  
+        
+     
+        `;
+
+      });
+
+      document.getElementById('customers').innerHTML = output1;
+
+    }
+  }
+
+ 
+  xhr.send();
+}
